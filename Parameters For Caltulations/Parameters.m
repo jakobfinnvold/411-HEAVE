75892041cb7d558ad2789d173532d6c6f140bc24
jakobfinnvold <55697129+jakobfinnvold@users.noreply.github.com>
2023-03-07 21:04:31 +0100

    Implemented waves in simulink

--- Parameters For Caltulations/Parameters.m
+++ Parameters For Caltulations/Parameters.m
@@ -31,3 +31,41 @@ h_payload_0 =15; % m
 h_payload_final = 0; % m
 h_seabed = 350; % m
 
+%% Sensors
+% Noise simulation
+sigma_w = 0.5; %m/s^2 (Standard deviation of noise)
+beta = 10000; % Positive constant [-]
+
+%% Payload parameters
+% mass already defined (8000kg)
+Volume_pl = 2; %m^3
+Area_pl = 1.5; %m^2
+C_D = 1.8; %[-] Drag coefficient
+
+syms rho vel
+F_drag = 0.5 * rho * vel^2 * C_D * Area_pl; % Density of sea water(surface) approx 1.025 kg/m^3, velocity comes from the simulation. 
+
+%% Seabed
+k_spring = 1.8e6; %N/m
+b_damper = 6.5e5; %Ns/m
+syms delta_x
+if delta_x < 0
+    F_sb = 0;
+else
+    F_sb = k_spring*delta_x + b_damper*vel*sqrt(delta_x); 
+end 
+
+%% Bouyant force
+% The force pushing upwards when something floats down in water. Dependend
+% on the fluid density, volume of the object falling and gravity
+g = 9.81; 
+F_boy = rho * Volume_pl * g;
+
+%% Electrical motor + Frequency converter
+% Insert selected motor and converter w/parameters here. 
+% Select these from MotorData.pdf and DriveData.pdf
+% First task should be to find a suitable motor...
+
+% Then, simulate the system using said choices. Implement field-oriented
+% control. This is only for electrical part. Should be switched out when
+% using speedgoat and PLC. (PLC should do the motion control). 
