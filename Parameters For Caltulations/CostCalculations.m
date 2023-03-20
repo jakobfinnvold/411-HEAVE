51586e6432b5f228c00c97ddef853a52b6b3e1c5
jakobfinnvold <55697129+jakobfinnvold@users.noreply.github.com>
2023-03-20 09:25:55 +0100

    New testspace

--- Parameters For Caltulations/CostCalculations.m
+++ Parameters For Caltulations/CostCalculations.m
@@ -1,11 +1,11 @@
 clear; close all; clc; 
-% Current selection: M3JP 315SMC 4
+% Current selection: M3JP 315SMC 4//
 % Motor
 omega_m = 1.5;
 PM_max = 200e3;
 poleselect = [2; 4; 6; 8]; % Number of poles
 n_p = poleselect(2,1);
-PM = 160e3;
+PM = 90e3;
 CM = omega_m * (1 + (PM/PM_max) + abs(n_p - 4)/4); 
 CM_max = omega_m * (1 + (PM_max/PM_max) + abs(8 - 4)/4); 
 
@@ -13,7 +13,7 @@ CM_max = omega_m * (1 + (PM_max/PM_max) + abs(8 - 4)/4);
 % Drive
 omega_c = 2.5; % [-]
 Pc_max = 200e3; % Watt
-P_c = 160e3; 
+P_c = 75e3; 
 C_c = omega_c*(1+P_c/Pc_max); 
 C_c_max = omega_c*(1+Pc_max/Pc_max); 
 
