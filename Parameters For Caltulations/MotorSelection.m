f8d6a77c8e8394ee3e8004d6d2bacb5b5d741a3d
jakobfinnvold <55697129+jakobfinnvold@users.noreply.github.com>
2023-03-22 17:25:19 +0100

    Added electrical parameters + finished motor tests

--- Parameters For Caltulations/MotorSelection.m
+++ Parameters For Caltulations/MotorSelection.m
@@ -10,6 +10,28 @@ for i = i_gb
     torque_gb(i) = torque_drum/i_gb(i);
 end 
 
+% M3KP 280SMB 4 for ig=5
+Rs = 0.04264; % From DC test
+
+Z_NL = 230.9/37.47;
+R_NL = 3546/(37.47^2);
+X_NL = sqrt(Z_NL^2 - R_NL^2);
+
+Z_LR = 28.87/181;
+R_LR = 451.8/(181^2);
+Rr_prime = R_LR - Rs; % reflected stator resistance
+X_LR = sqrt(Z_LR^2 - R_LR^2);
+X_LR_50Hz = 50/12.5 * X_LR;
+
+% Using Design Class B Motor
+X1 = 0.4 * X_LR_50Hz
+X2 = 0.6 * X_LR_50Hz
+
+XM = X_NL - X1;
+
+
+
+
 
 
 
